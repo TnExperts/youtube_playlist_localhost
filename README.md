b0a76939e5ed3f600d00c421cc8ccb18c52b0a2e
Marcedo <Marcedo@HabMalNeFrage.de>
2018-10-12 20:12:19 +0200

    Version 0.11

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # youtube_playlist_localhost
 	
-youtube_raw: -> depreceated
+youtube_raw: -> depreceated. 
+
+youtube_api:
 - Streams a YouTube Link, an ID, or even a Playlist to a VLC Instance.
 - Make it available on http://localhost:8080/gogo.ts (for use with eg LiveStreaming Apps aka OBS)
 - Removed dependency to "youtube-dl" and "ffmpeg"
-
-youtube_api:
-- Youtube switched some internal logic. Currently testing an api-Key based Method to fetch playlist items.
+- Youtube switched some internal logic. So rewrote the playlist fetcher to use an api-Key based approach.
 
 younow:
 - A simple YouNow Comment Interface for use with OBS Text Feature.  
@@ -17,10 +17,7 @@ stream_to_localhost:
 -------------------
 
 -> OBS Post: https://obsproject.com/forum/resources/obs_adfree_youtube_playlists.642/
-
 -> Author: Marcedo@habMalNeFrage.de
-
 -> Date Okt 2018
-
 -> License: FreeBSD 3Clause
 
--- youtube_api/yt_get_playlist_apikey.php
+++ youtube_api/yt_get_playlist_apikey.php
@@ -1,8 +1,12 @@
 <?php
 
-// Simple and Fast YouTube Playlist videoid fetcher.
-//  alpha test version v0.10 :) - Marcedo@habMalNeFrage.de
-// License MIT
+// Simple and Fast YouTube PlaylistItems fetcher.
+// https://github.com/arjunae/youtube_playlist_localhost
+// alpha test version v0.11 :) - Marcedo@habMalNeFrage.de
+// License BSD3Clause 
+
+// PlaylistItems API Description :
+// https://developers.google.com/youtube/v3/docs/playlistItems/list
 
 	require_once "class.http.api.php";
 
@@ -11,10 +15,9 @@
 	ini_set("memory_limit", "4000M");
 	ini_set("max_execution_time", 3600*5);
 
-	// NOTE: That APIv3 Key was created using the following:
-	// guide: [youtubeapi-v3](https://developers.google.com/youtube/v3/getting-started)
+	// NOTE: That APIv3 Key was created using the following guide: [youtubeapi-v3](https://developers.google.com/youtube/v3/getting-started)
 	$apikey = "AIzaSyBeeymyfYDFB1xaiHDH4lYtbSeeA0dG-Gg";
-	// To avoid permanet abuse, ill revoke that somwhen in the future. Feel Free to create your own.
+	// To avoid permanent abuse, ill revoke that somwhen in the future. Feel Free to create your own :)
 	
 	// Parse argv - require console and minimum one parameter. 
 	if(PHP_SAPI !== 'cli') { 
@@ -30,7 +33,7 @@
 	}	
 
 	// Differenciate a single Video ID from a Playlist ID
-	//https://linuxpanda.wordpress.com/2013/07/24/ultimate-best-regex-pattern-to-get-grab-parse-youtube-video-id-from-any-youtube-link-url/
+	// https://linuxpanda.wordpress.com/2013/07/24/ultimate-best-regex-pattern-to-get-grab-parse-youtube-video-id-from-any-youtube-link-url/
 
 	// Video id is 11 characters in length
 	$video_pattern = '~(?:http|https|)(?::\/\/|)(?:www.|)(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/ytscreeningroom\?v=|\/feeds\/api\/videos\/|\/user\S*[^\w\-\s]|\S*[^\w\-\s]))([\w\-]{11})[a-z0-9;:@#?&%=+\/\$_.-]*~i';
@@ -58,22 +61,31 @@
 		$feedURL= $url;
 	}
 
-	// Now, ask Youtubes v3Api about the Playlists contents
+	// playlist id sample https://www.youtube.com/watch?v=g3ml_WCpbsg&list=RDg3ml_WCpbsg
+	// $playlistId = "RDg3ml_WCpbsg";
+	
+	// Now, ask Youtubes v3Api about the Playlists contents.
+	$playlistApi="https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=40&playlistId=";
 	$http=new httpServicesAPI();
 	$http->do_set_options();
 
-	// playlist id from https://www.youtube.com/watch?v=g3ml_WCpbsg&list=RDg3ml_WCpbsg
-	//$playlistId = "RDg3ml_WCpbsg";
-	$restquery = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=".$playlistId."&key=".$apikey;
-	$json=json_decode($http->get($restquery));
-	$http->close();
-
-	// TODO: Parse all pages from the response within an outer loop
-	//print_r("Results: ".$json->pageInfo->totalResults.PHP_EOL);
+	// Now iterate and parse through all resultsets from the response.
+	$restquery = $playlistApi.$playlistId."&key=".$apikey;
 	
-	foreach($json->items as $key) {
-		print $key->snippet->resourceId->videoId.";";
-		print("'".$key->snippet->title."'".PHP_EOL);
-	}
-
+	$nextPageToken=0;
+	do {
+				$json=json_decode($http->get($restquery));	
+				foreach($json->items as $key) {
+					print ($key->snippet->resourceId->videoId."'".$key->snippet->title."'".PHP_EOL);
+				}
+				if (property_exists($json,"nextPageToken")) {
+					$nextPageToken=$json->nextPageToken;
+					$restquery = $playlistApi.$playlistId."&pageToken=".$nextPageToken."&key=".$apikey;
+				} else { 
+					break; 
+				}
+	} while ($nextPageToken=!0);
+		
+	$http->close();
+	
 ?>
