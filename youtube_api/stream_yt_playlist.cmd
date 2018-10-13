81e6143822e45d3379a94a38abaed492f580d1ff
Marcedo <Marcedo@HabMalNeFrage.de>
2018-10-13 11:05:04 +0200

    Add a funny Batch based WatchDog :)

--- youtube_api/stream_yt_playlist.cmd
+++ youtube_api/stream_yt_playlist.cmd
@@ -4,15 +4,17 @@ REM removed dependency to "youtube-dl" and "ffmpeg"
 REM Oct 2018, Marcedo@habMalNeFrage.de
 REM .Engeneered to survive BlackHoles :)
 
+setlocal enabledelayedexpansion
 set PATH=%PATH%;C:\Program Files\VideoLAN\VLC
 set php_bin=..\php\php.exe 
 set link=%1
+set windowTitle=_Playlist_Streamer_
+TITLE %windowTitle%
 
-setlocal enabledelayedexpansion
 echo        --== Route a youtube htttp stream via localhost ==--
 IF ["%link%"] equ [""] SET /P Link=YouTube Link , Playlist or ID ? (empty to use last generated list) : 
-TASKKILL /IM VLC.exe 1>NUL 2>NUL
-TASKKILL /IM php.exe 1>NUL 2>NUL
+TASKKILL /F /IM  VLC.exe 1>NUL 2>NUL
+TASKKILL /F /IM  php.exe 1>NUL 2>NUL
 
 IF ["%Link%"] neq [""]  ECHO ...Fetching Playlist Urls to yt_playlist.lst... 
 IF ["%Link%"] neq [""]  %php_bin% yt_get_playlist_apikey.php "%Link%" > yt_playlist.txt
@@ -24,17 +26,8 @@ for /f "tokens=2 delims=:" %%i in ('find /V /C "" yt_playlist.txt') do echo  ..F
 :: WorkAround  for VLC Version 3.01 - Need to route the stream via localhost
 ::  OBS-Playlist Bug. Should be fixed in 3.03
 :: todo -> Watchdog cleaning up when execution of the steering batchfile ends. 
-
-echo @echo off >vlc_start.bat
-echo :wait  >> vlc_start.bat
-echo if not EXIST stream.ts goto :wait >> vlc_start.bat
-echo REM simple trick to just send a file as a ts stream - without further processing. >>vlc_start.bat
-echo start /MIN vlc.exe -I dummy  --one-instance --playlist-enqueue "stream.ts" --loop --sout-keep --sout=#gather:http{dst=:8080/gogo.ts} >>vlc_start.bat
-echo REM # Using FlashVideo can be seen as "more compatible" but also does requires more processing time.  >>vlc_start.bat
-echo REM # start /MIN /ABOVENORMAL vlc.exe -I dummy --no-interact  --one-instance --playlist-enqueue "stream.ts" --loop --sout-keep --sout=#gather:http{mux=ffmpeg{mux=flv},dst=:8080/bla}  >>vlc_start.bat
-echo exit >>vlc_start.bat
 ECHO  ..Init VLC-OBS Bridge via http://localhost:8080/gogo.ts 
-start  /MIN /B vlc_start.bat
+start /MIN cmd.exe /c wd.cmd
 
 ::ECHO  ..Streaming YouNow Comments to yn-comments.txt
 ::start  younow\yn_comments.cmd
@@ -56,9 +49,8 @@ REM # yt_get_prot.php ::> https://gist.github.com/arjunae/6737ecf40956efa3fe4c4d
 REM # Quality : mp4-640x360 (Format No 18). Change the php script if you need the Data in another Format.
 for /f %%i in ('%php_bin% yt_get_protID.php %youtube_id%') do set Link=%%i
 REM echo "%Link% : %youtube_id%"
-start /WAIT /MIN /ABOVENORMAL vlc.exe --vout=dummy --volume 0  "%Link%" --sout=#duplicate{dst=std{access=file,mux=ts,dst=stream.ts},dst=display} vlc://quit
-exit /b 0 
-:end_sub
+start /WAIT /MIN /ABOVENORMAL vlc.exe "%Link%" --sout=#duplicate{dst=std{access=file,mux=ts,dst=stream.ts},dst=display} vlc://quit
+exit /b 0
 
 :err_api
 echo ...Error connecting to youtube API. Exitcode: %ERRORLEVEL% 
---
+++ youtube_api/wd.cmd
@@ -0,0 +1,37 @@
+@echo off
+REM = A funny BatchBased Process Watchdog.
+REM = Ends %DEPEND% when %WATCH% ends.
+REM = requires winVersion >= Vista 
+REM = LIC BSD3CLause
+
+set watch="_Playlist_Streamer_"
+set depend="vlc_start.bat"
+
+REM = Create the depend BatchJob
+echo @echo off >vlc_start.bat
+echo :wait  >> vlc_start.bat
+echo if not EXIST stream.ts goto :wait >> vlc_start.bat
+echo REM simple trick to just send a file as a ts stream - without further processing. >>vlc_start.bat
+echo start /MIN vlc.exe -I dummy  --one-instance --playlist-enqueue "stream.ts" --loop --sout-keep --sout=#gather:http{dst=:8080/gogo.ts} >>vlc_start.bat
+echo REM # Using FlashVideo can be seen as "more compatible" but also does requires more processing time.  >>vlc_start.bat
+echo REM # start /MIN /ABOVENORMAL vlc.exe -I dummy --no-interact  --one-instance --playlist-enqueue "stream.ts" --loop --sout-keep --sout=#gather:http{mux=ffmpeg{mux=flv},dst=:8080/bla}  >>vlc_start.bat
+echo exit >>vlc_start.bat
+
+start /b /min vlc_start.bat
+for /F "tokens=1,2* delims=, " %%a in ('tasklist /v /FO:CSV ^| find %DEPEND%') do ( set PIDD=%%b )
+echo Watchdog %watch%--%depend%
+
+REM Wait for the Watched Batch Job to finish.
+:loop
+TIMEOUT /T 1 /NOBREAK 1>NUL
+set PIDW=0
+for /F "tokens=1,2* delims=, " %%a in ('tasklist /v /FO:CSV ^| find %WATCH%') do (
+set PIDW=%%b
+)
+if [%PIDW%] NEQ [0] goto :loop
+
+REM Now end the depend Job.
+echo %WATCH% has ended , stopping %PIDD%
+TASKKILL /F /IM  VLC.exe
+del /F vlc_start.bat 1>NUL 2>NUL
+del /F stream.ts 1>NUL 2>NUL
